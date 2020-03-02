02c7a3fa01e525bf9e86f59d7883c9bd735a96a0
Linh@home <qnguyen@intracen.org>
2020-03-02 19:07:11 +0100

    Add prod config

--- webpack.config.js
+++ webpack.config.js
@@ -3,112 +3,105 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
-module.exports = function(env) {
-  return {
-    entry: {
-      main: ["./src/index.js"],
+module.exports = {
+  entry: {
+    main: ["./src/index.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "app/[name].js",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    historyApiFallback: {
+      index: "index.html",
     },
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/",
-      filename: "app/[name].js",
-    },
-    devServer: {
-      contentBase: path.resolve(__dirname, "dist"),
-      historyApiFallback: {
-        index: "index.html",
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ options: {} }),
+    new HtmlWebpackPlugin({
+      template: `./public/template.html`,
+    }),
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 5120,
+      minRatio: 0.8,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        loader: "eslint-loader",
       },
-    },
-    plugins: [
-      new webpack.LoaderOptionsPlugin({ options: {} }),
-      new HtmlWebpackPlugin({
-        template: `./public/template.html`,
-      }),
-      new WorkboxPlugin.InjectManifest({
-        swSrc: path.resolve(__dirname, "public/service-worker.js"),
-        swDest: "sw.js",
-        exclude: ["web.config", ".htaccess"],
-      }),
-      new CompressionPlugin({
-        filename: "[path].gz[query]",
-        algorithm: "gzip",
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 5120,
-        minRatio: 0.8,
-      }),
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|pdf)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets",
+          name: "[hash:8].[ext]",
+        },
+      },
+      {
+        test: /\.(config|ico)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        test: /\.htaccess$/,
+        loader: "file-loader",
+        options: {
+          name: "[name]",
+        },
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "data",
+          name: "[name].[ext]",
+        },
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+      {
+        type: "javascript/auto",
+        test: /\.json$/,
+        exclude: /i18n/,
+        loader: "file-loader",
+        options: {
+          outputPath: "data",
+          name: "[name].[ext]",
+        },
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          enforce: "pre",
-          loader: "eslint-loader",
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|pdf)$/,
-          loader: "file-loader",
-          options: {
-            outputPath: "assets",
-            name: "[hash:8].[ext]",
-          },
-        },
-        {
-          test: /\.(config|ico)$/,
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-          },
-        },
-        {
-          test: /\.htaccess$/,
-          loader: "file-loader",
-          options: {
-            name: "[name]",
-          },
-        },
-        {
-          test: /\.(csv|tsv)$/,
-          loader: "file-loader",
-          options: {
-            outputPath: "data",
-            name: "[name].[ext]",
-          },
-        },
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: "javascript/auto",
-        },
-        {
-          type: "javascript/auto",
-          test: /\.json$/,
-          exclude: /i18n/,
-          loader: "file-loader",
-          options: {
-            outputPath: "data",
-            name: "[name].[ext]",
-          },
-        },
-      ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
     },
-    resolve: {
-      extensions: ["*", ".js", ".jsx"],
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-      alias: {
-        "react-dom": "@hot-loader/react-dom",
-      },
-    },
-  };
+  },
 };
---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const config = require("./webpack.config.js");
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = merge(config, {
+  mode: "production",
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ options: {} }),
+    new HtmlWebpackPlugin({
+      template: `./public/template.prod.html`,
+    }),
+    new WorkboxPlugin.InjectManifest({
+      swSrc: path.resolve(__dirname, "public/service-worker.js"),
+      swDest: "sw.js",
+      exclude: ["web.config", ".htaccess"],
+    }),
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 5120,
+      minRatio: 0.8,
+    }),
+  ],
+});
