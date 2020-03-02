495a19929aa2f5bb5715b0661d7383b73eff773b
Linh@home <qnguyen@intracen.org>
2020-03-03 00:39:05 +0100

    Polish the layout selector

--- src/modules/text2bettertext/components/StatusBar/LayoutSelector.js
+++ src/modules/text2bettertext/components/StatusBar/LayoutSelector.js
@@ -8,8 +8,17 @@ import { selectCurrentLayout } from "../../selectors/layout";
 import Button from "shared-components/Button";
 
 const LayoutSelector = styled.div`
+  display: flex;
+  align-items: center;
+
   & > span {
     color: rgba(255, 255, 255, 0.5);
+  }
+
+  .btns {
+    display: inline-block;
+    border-radius: 10px 10px 10px 0;
+    overflow: hidden;
   }
 `;
 
@@ -20,20 +29,22 @@ export default () => {
   return (
     <LayoutSelector>
       <span>layout&nbsp;&nbsp;</span>
-      {["⫞", "⫟", "=", "∥"].map(layout => (
-        <Button
-          icon
-          key={layout}
-          active={layout === currentLayout}
-          onClick={() => dispatch(setLayoutCurrent(layout))}
-        >
-          {layout === "∥" ? (
-            <span style={{ transform: "rotate(90deg)", display: "inline-block" }}>=</span>
-          ) : (
-            layout
-          )}
-        </Button>
-      ))}
+      <div className="btns">
+        {["⫞", "⫟", "=", "∥"].map(layout => (
+          <Button
+            icon
+            key={layout}
+            active={layout === currentLayout}
+            onClick={() => dispatch(setLayoutCurrent(layout))}
+          >
+            {layout === "∥" ? (
+              <span style={{ transform: "rotate(90deg)", display: "inline-block" }}>=</span>
+            ) : (
+              layout
+            )}
+          </Button>
+        ))}
+      </div>
     </LayoutSelector>
   );
 };
