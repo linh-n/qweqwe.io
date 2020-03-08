240bc02924d3b65122d33e725dd6ea2b765f31d4
Linh@home <qnguyen@intracen.org>
2020-03-08 01:56:05 +0100

    Add defaults file to store default values

---
+++ src/modules/text2bettertext/defaults.js
@@ -0,0 +1,10 @@
+export const defaultState = {
+  sourceText: "",
+  templateText: "",
+  layout: {
+    currentLayout: "⫞",
+    isEditing: false,
+    splitPaneVertical: 50,
+    splitPaneHorizontal: 50,
+  },
+};
--- src/modules/text2bettertext/reducer.js
+++ src/modules/text2bettertext/reducer.js
@@ -1,24 +1,15 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
+import { defaultState } from "./defaults";
 
 export const setSourceText = createAction("[Text transformer] Set original text");
 export const setTemplateText = createAction("[Text transformer] Set template text");
-export const addFunction = createAction("[Text transformer] Add function");
+
+export const setFunction = createAction("[Text transformer] Set function");
 
 export const setLayoutCurrent = createAction("[Text transformer] Set layout");
 export const setLayoutIsEditing = createAction("[Text transformer] Set layout isEditing state");
 export const setLayoutSplitPane1 = createAction("[Text transformer] Set layout split pane 1");
 export const setLayoutSplitPane2 = createAction("[Text transformer] Set layout split pane 2");
-
-const defaultState = {
-  sourceText: "",
-  templateText: "",
-  layout: {
-    currentLayout: "⫞",
-    isEditing: false,
-    splitPaneVertical: 50,
-    splitPaneHorizontal: 50,
-  },
-};
 
 export default createReducer(
   { ...defaultState },
