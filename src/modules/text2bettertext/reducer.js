b2e26ab40ae75b0f96164fa1a70eb569841d65d7
Linh@home <qnguyen@intracen.org>
2020-03-26 15:58:01 +0100

    Add quick sample

--- src/modules/text2bettertext/reducer.js
+++ src/modules/text2bettertext/reducer.js
@@ -9,8 +9,9 @@ export const setLayoutSplitPane1 = createAction("[Text transformer] Set layout s
 export const setLayoutSplitPane2 = createAction("[Text transformer] Set layout split pane 2");
 
 const defaultState = {
-  sourceText: "",
-  templateText: "",
+  sourceText:
+    "2\t4\t4\tAF\tAFG\tAfghanistan\tAfghanistan\tAfganistán\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tأفغانستان\t \n  3 \t8\t8\tAL\tALB\tAlbania\tAlbanie\tAlbania\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tألبانيا\t \n  4 \t12\t12\tDZ\tDZA\tAlgeria\tAlgérie\tArgelia\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tالجزائر\t \n  5 \t16\t16\tAS\tASM\tAmerican Samoa\tSamoa américaines\tSamoa americana\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tساموا الأمريكية\t ",
+  templateText: "update Countries set NameFr = '{7}' where CountryCode = '{5}';",
   layout: {
     currentLayout: "⫞",
     isEditing: false,
