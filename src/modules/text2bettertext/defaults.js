0c7fd2ac30fcd503b3605b868fe992f0b1d3a5e8
Linh@home <qnguyen@intracen.org>
2020-03-26 17:28:52 +0100

    Update trendy sample

--- src/modules/text2bettertext/defaults.js
+++ src/modules/text2bettertext/defaults.js
@@ -1,7 +1,7 @@
 export const defaultState = {
   sourceText:
-    "2\t4\t4\tAF\tAFG\tAfghanistan\tAfghanistan\tAfganistán\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tأفغانستان\t \n  3 \t8\t8\tAL\tALB\tAlbania\tAlbanie\tAlbania\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tألبانيا\t \n  4 \t12\t12\tDZ\tDZA\tAlgeria\tAlgérie\tArgelia\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tالجزائر\t \n  5 \t16\t16\tAS\tASM\tAmerican Samoa\tSamoa américaines\tSamoa americana\t0\t1809-01-01\t12/31/9999\t1/1/1900\t12/31/9999\tساموا الأمريكية\t ",
-  templateText: "update Countries set NameFr = '{G}' where CountryCode = '{E}';",
+    "China\t81,285\t+67\t3,287\t+6\t74,051\t3,947\t1,235\t56\t2\nItaly\t74,386\t7,503\t9,362\t57,521\t3,489\t1,230\t124\nUSA\t68,814\t+603\t1,037\t+10\t428\t67,349\t1,455\t208\t3\nSpain\t56,188\t+6,673\t4,089\t+442\t7,015\t45,084\t3,166\t1,202\t87\nGermany\t40,421\t+3,098\t229\t+23\t3,959\t36,233\t23\t482\t3\nIran\t29,406\t+2,389\t2,234\t+157\t10,457\t16,715\t2,746\t350\t27\nFrance\t25,233\t1,331\t3,900\t20,002\t2,827\t387\t20\nSwitzerland\t11,316\t+419\t171\t+18\t131\t11,014\t141\t1,308\t20",
+  templateText: "{\n  country: '{A}',\n  infected: '{B}',\n  died: '{D}',\n},",
   functions: [],
   layout: {
     currentLayout: "⫞",
